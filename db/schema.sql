6c942a0ff2a43c2856efa6f7c1bd3d327765f0f5
Stacy <stacytrent@gmail.com>
2016-11-16 12:33:30 -0500

    still a work in progress, didn't get far in to this

--- db/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers(
-id int AUTO_INCREMENT,
-PRIMARY KEY (id),
-burger_name varchar(50) NOT NULL,
-devoured BOOLEAN default false,
-date TIMESTAMP);
--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers(burger_name) VALUES ('Big Mac');
-INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken');
-INSERT INTO burgers(burger_name) VALUES ('A1 Thick N Hearty');
-INSERT INTO burgers(burger_name) VALUES ('The Whopper');
-INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
